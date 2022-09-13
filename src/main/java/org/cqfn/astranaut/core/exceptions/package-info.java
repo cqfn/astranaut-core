8825c0a016109c916eb11ac89463de6e37a76212
Polina Volkhontseva <polina.volkhontseva@huawei.com>
2022-09-13 15:55:32 +0300

    Fixes in exceptions

--- src/main/java/org/cqfn/astranaut/core/exceptions/package-info.java
+++ src/main/java/org/cqfn/astranaut/core/exceptions/package-info.java
@@ -21,43 +21,11 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package org.cqfn.astranaut.core.utils;
-
-import java.io.File;
-import java.io.IOException;
-import org.cqfn.astranaut.core.Node;
-import org.cqfn.astranaut.core.exceptions.WrongFileExtension;
 
 /**
- * Visualizer of trees.
+ * This package contains a base helper class for custom exceptions and common exceptions
+ * for projects that use the core as a dependency.
  *
- * @since 1.0.2
+ * @since 1.0.3
  */
-public class TreeVisualizer {
-    /**
-     * The tree to be visualized.
-     */
-    private final Node tree;
-
-    /**
-     * Constructor.
-     * @param tree The tree to be visualized
-     */
-    public TreeVisualizer(final Node tree) {
-        this.tree = tree;
-    }
-
-    /**
-     * Renders a DOT text of the tree, renders an image from it and
-     * saves it to the specified file.
-     * @param file A file of the AST visualization
-     * @throws IOException If an error during input or output actions occurs
-     * @throws WrongFileExtension If an error during visualization occurs
-     */
-    public void visualize(final File file) throws IOException, WrongFileExtension {
-        final DotRender render = new DotRender(this.tree);
-        final String dot = render.render();
-        final ImageRender image = new ImageRender(dot);
-        image.render(file);
-    }
-}
+package org.cqfn.astranaut.core.exceptions;
