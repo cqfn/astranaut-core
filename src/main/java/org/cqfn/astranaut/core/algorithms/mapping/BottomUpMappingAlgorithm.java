67117d7e5a644e0adb210acfdd15b678af3f5186
Ivan Kniazkov <ivan@kniazkov.com>
2024-02-14 15:33:02 +0300

    Mapping by identical hashes from bottom to top

--- src/main/java/org/cqfn/astranaut/core/algorithms/mapping/BottomUpMappingAlgorithm.java
+++ src/main/java/org/cqfn/astranaut/core/algorithms/mapping/BottomUpMappingAlgorithm.java
@@ -23,10 +23,13 @@
  */
 package org.cqfn.astranaut.core.algorithms.mapping;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import org.cqfn.astranaut.core.Node;
 import org.cqfn.astranaut.core.algorithms.Depth;
 import org.cqfn.astranaut.core.algorithms.hash.AbsoluteHash;
@@ -43,7 +46,7 @@ class BottomUpMappingAlgorithm {
     /**
      * Set of node hashes.
      */
-    private final Hash hash;
+    private final Hash hashes;
 
     /**
      * Set of node depths.
@@ -51,14 +54,19 @@ class BottomUpMappingAlgorithm {
     private final Depth depth;
 
     /**
+     * Relationship of the nodes to their parents.
+     */
+    private final Map<Node, Node> parents;
+
+    /**
      * Not yet processed nodes from the 'left' tree.
      */
-    private final Set<ExtNode> left;
+    private final Set<Node> left;
 
     /**
      * Not yet processed nodes from the 'right' tree.
      */
-    private final Set<ExtNode> right;
+    private final Set<Node> right;
 
     /**
      * Left-to-right mapping.
@@ -70,15 +78,15 @@ class BottomUpMappingAlgorithm {
      */
     private final Map<Node, Node> rtl;
 
-
     /**
      * Constructor.
      * @param left Root node of the 'left' tree
      * @param right Root node of the 'right' tree
      */
     BottomUpMappingAlgorithm(final Node left, final Node right) {
-        this.hash = new AbsoluteHash();
+        this.hashes = new AbsoluteHash();
         this.depth = new Depth();
+        this.parents = new HashMap<>();
         this.left = this.createNodeSet(left);
         this.right = this.createNodeSet(right);
         this.ltr = new HashMap<>();
@@ -89,7 +97,8 @@ class BottomUpMappingAlgorithm {
      * Performs the mapping.
      */
     void execute() {
-
+        final Map<Node, List<Node>> draft = this.performInitialMapping();
+        this.absorbLargestSubtrees(draft);
     }
 
     /**
@@ -99,13 +108,13 @@ class BottomUpMappingAlgorithm {
     Mapping getResult() {
         return new Mapping() {
             @Override
-            public Node getRight(final Node left) {
-                return BottomUpMappingAlgorithm.this.ltr.get(left);
+            public Node getRight(final Node node) {
+                return BottomUpMappingAlgorithm.this.ltr.get(node);
             }
 
             @Override
-            public Node getLeft(Node right) {
-                return BottomUpMappingAlgorithm.this.rtl.get(right);
+            public Node getLeft(final Node node) {
+                return BottomUpMappingAlgorithm.this.rtl.get(node);
             }
         };
     }
@@ -113,10 +122,10 @@ class BottomUpMappingAlgorithm {
     /**
      * Creates an initial set of nodes suitable for processing from the tree.
      * @param root The root of the tree
-     * @return Set of extended nodes
+     * @return Set of nodes
      */
-    private Set<ExtNode> createNodeSet(final Node root) {
-        final Set<ExtNode> set = new HashSet<>();
+    private Set<Node> createNodeSet(final Node root) {
+        final Set<Node> set = new HashSet<>();
         this.createNodeSet(root, null, set);
         return set;
     }
@@ -127,59 +136,87 @@ class BottomUpMappingAlgorithm {
      * @param parent The current node parent
      * @param set The resulting set
      */
-    private void createNodeSet(final Node node, final Node parent, final Set<ExtNode> set) {
-        set.add(new ExtNode(node, parent));
-        node.forEachChild(child -> BottomUpMappingAlgorithm.this.createNodeSet(child, node, set));
+    private void createNodeSet(final Node node, final Node parent, final Set<Node> set) {
+        set.add(node);
+        this.parents.put(node, parent);
+        node.forEachChild(child -> this.createNodeSet(child, node, set));
     }
 
     /**
-     * Extended node containing information required for mapping.
-     *
-     * @since 1.1.0
+     * Performs hash calculation of nodes from the 'right' set.
+     * @return The hash relation to the list of nodes that have such a hash
      */
-    private class ExtNode {
-        /**
-         * The node itself.
-         */
-        private final Node node;
-
-        /**
-         * The parent of the node.
-         */
-        private final Node parent;
-
-        /**
-         * Constructor.
-         * @param node The node itself
-         * @param parent The parent of the node
-         */
-        ExtNode(final Node node, final Node parent) {
-            this.node = node;
-            this.parent = parent;
+    private Map<Integer, List<Node>> calculateRightHashes() {
+        final Map<Integer, List<Node>> result = new TreeMap<>();
+        for (final Node node : this.right) {
+            final int hash = this.hashes.calculate(node);
+            final List<Node> list =
+                result.computeIfAbsent(hash, k -> new ArrayList<>(1));
+            list.add(node);
         }
+        return result;
+    }
 
-        Node getNode() {
-            return this.node;
+    /**
+     * Performs initial (draft) node mapping.
+     * @return Relationships of nodes to lists of nodes to which they can be mapped to
+     */
+    private Map<Node, List<Node>> performInitialMapping() {
+        final Map<Node, List<Node>> result = new HashMap<>();
+        final Map<Integer, List<Node>> relation = this.calculateRightHashes();
+        for (final Node node : this.left) {
+            final int hash = this.hashes.calculate(node);
+            final List<Node> list = relation.get(hash);
+            if (list != null) {
+                result.put(node, list);
+            }
         }
+        return result;
+    }
 
-        Node getParent() {
-            return this.parent;
+    /**
+     * Selects the largest size subtrees from the initial node relation and maps them.
+     * @param draft Initial node relation
+     */
+    private void absorbLargestSubtrees(final Map<Node, List<Node>> draft) {
+        final List<Node> sorted = new ArrayList<>(draft.keySet());
+        sorted.sort(
+            (first, second) -> -Integer.compare(
+                this.depth.calculate(first),
+                this.depth.calculate(second)
+            )
+        );
+        for (final Node node : sorted) {
+            final List<Node> related = draft.get(node);
+            if (related != null && related.size() == 1 && !this.ltr.containsKey(node)) {
+                this.mapSubtreesWithTheSameHash(node, related.get(0), draft);
+            }
+            if (draft.isEmpty()) {
+                break;
+            }
         }
+    }
 
-        /**
-         * Calculates the hash of the node.
-         * @return Node hash
-         */
-        int getHash() {
-            return BottomUpMappingAlgorithm.this.hash.calculate(this.node);
-        }
-
-        /**
-         * Calculates the depth of the node.
-         * @return Node depth
-         */
-        int getDepth() {
-            return BottomUpMappingAlgorithm.this.depth.calculate(this.node);
+    /**
+     * Maps subtrees with the same hash, adding the corresponding nodes to the resulting
+     * collections and removing them from the initial mapping.
+     * @param node Left node
+     * @param related Related node to the left node
+     * @param draft Initial node relation
+     */
+    private void mapSubtreesWithTheSameHash(
+        final Node node,
+        final Node related,
+        final Map<Node, List<Node>> draft) {
+        assert this.hashes.calculate(node) == this.hashes.calculate(related);
+        draft.remove(node);
+        this.ltr.put(node, related);
+        this.rtl.put(related, node);
+        final int count = node.getChildCount();
+        for (int index = 0; index < count; index = index + 1) {
+            final Node first = node.getChild(index);
+            final Node second = related.getChild(index);
+            this.mapSubtreesWithTheSameHash(first, second, draft);
         }
     }
 }
