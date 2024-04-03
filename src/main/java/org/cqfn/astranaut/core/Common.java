9c74e2bc6149f6ed31f68bf17c5130a8c839b35a
Ivan Kniazkov <33510482+kniazkov@users.noreply.github.com>
2024-04-03 11:39:25 +0300

    Update version info.java

--- src/main/java/org/cqfn/astranaut/core/Common.java
+++ src/main/java/org/cqfn/astranaut/core/Common.java
@@ -32,7 +32,7 @@ public final class Common {
     /**
      * The core version.
      */
-    public static final String VERSION = "1.0.6";
+    public static final String VERSION = "1.1.4";
 
     /**
      * Private constructor.
