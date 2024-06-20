b0f6704ad57c2761fc5c94baff2733d5edf05873
Ivan Kniazkov <ivan@kniazkov.com>
2024-06-20 15:20:25 +0300

    Class representing syntax tree

---
+++ src/main/java/org/cqfn/astranaut/core/base/Tree.java
@@ -0,0 +1,90 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2024 Ivan Kniazkov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cqfn.astranaut.core.base;
+
+import org.cqfn.astranaut.core.Node;
+import org.cqfn.astranaut.core.Type;
+
+/**
+ * A syntax tree, which represents the hierarchical structure of source code,
+ *  depicting the syntactic structure and relationships between code elements.
+ *
+ * @since 2.0.0
+ */
+public class Tree {
+    /**
+     * Root node of the tree.
+     */
+    private final Node root;
+
+    /**
+     * Constructor.
+     * @param root Root node the tree
+     */
+    public Tree(final Node root) {
+        this.root = root;
+    }
+
+    /**
+     * Returns the root node of the tree.
+     * @return Root node of the tree
+     */
+    public Node getRoot() {
+        return this.root;
+    }
+
+    /**
+     * Returns the name of the programming language from which the source code,
+     *  used to construct this tree, was written.
+     * @return The programming language name or an empty string if no language is specified
+     *  in any of the nodes.
+     */
+    public String getLanguage() {
+        return Tree.getLanguage(this.root);
+    }
+
+    @Override
+    public final String toString() {
+        return this.root.toString();
+    }
+
+    /**
+     * Analyzes the node and its children (recursively) to determine if a programming language
+     *  name is specified.
+     * @param node Node for analysis
+     * @return The programming language name or an empty string if no language is specified
+     *  in any of the child nodes.
+     */
+    protected static String getLanguage(final Node node) {
+        final Type type = node.getType();
+        String language = type.getProperty("language");
+        if (language.isEmpty()) {
+            final int count = node.getChildCount();
+            for (int index = 0; language.isEmpty() && index < count; index = index + 1) {
+                language = Tree.getLanguage(node.getChild(index));
+            }
+        }
+        return language;
+    }
+}
---
+++ src/test/java/org/cqfn/astranaut/core/base/TreeTest.java
@@ -0,0 +1,167 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2024 Ivan Kniazkov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cqfn.astranaut.core.base;
+
+import java.util.Collections;
+import java.util.List;
+import org.cqfn.astranaut.core.Builder;
+import org.cqfn.astranaut.core.ChildDescriptor;
+import org.cqfn.astranaut.core.DraftNode;
+import org.cqfn.astranaut.core.EmptyFragment;
+import org.cqfn.astranaut.core.Fragment;
+import org.cqfn.astranaut.core.Node;
+import org.cqfn.astranaut.core.Type;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This test suite tests the {@link Tree} class.
+ *
+ * @since 2.0.0
+ */
+class TreeTest {
+    @Test
+    void testBaseInterface() {
+        final String language = "quenya";
+        final String description = "A(B, C)";
+        final DraftNode.Constructor ctor = new DraftNode.Constructor();
+        ctor.setName("A");
+        ctor.addChild(DraftNode.createByDescription("B"));
+        ctor.addChild(new TestNode("C", language));
+        final Tree tree = new Tree(ctor.createNode());
+        Assertions.assertEquals(description, tree.getRoot().toString());
+        Assertions.assertEquals(description, tree.toString());
+        Assertions.assertEquals(language, tree.getLanguage());
+    }
+
+    /**
+     * Node implementation for testing purposes.
+     *
+     * @since 2.0.0
+     */
+    private static final class TestNode implements Node {
+        /**
+         * The type of the node.
+         */
+        private final Type type;
+
+        /**
+         * Constructor.
+         * @param name The name of the type
+         * @param language The name of the programming language
+         */
+        private TestNode(final String name, final String language) {
+            this.type = new TestType(name, language);
+        }
+
+        @Override
+        public Fragment getFragment() {
+            return EmptyFragment.INSTANCE;
+        }
+
+        @Override
+        public Type getType() {
+            return this.type;
+        }
+
+        @Override
+        public String getData() {
+            return "";
+        }
+
+        @Override
+        public int getChildCount() {
+            return 0;
+        }
+
+        @Override
+        public Node getChild(final int index) {
+            return null;
+        }
+
+        @Override
+        public String toString() {
+            return this.type.getName();
+        }
+    }
+
+    /**
+     * Type implementation for testing purposes.
+     *
+     * @since 2.0.0
+     */
+    private static final class TestType implements Type {
+        /**
+         * The name of the type.
+         */
+        private final String name;
+
+        /**
+         * The name of the programming language from which the source code, used to construct
+         *  this tree, was written.
+         */
+        private final String language;
+
+        /**
+         * Constructor.
+         * @param name The name of the type
+         * @param language The name of the programming language
+         */
+        private TestType(final String name, final String language) {
+            this.name = name;
+            this.language = language;
+        }
+
+        @Override
+        public String getName() {
+            return this.name;
+        }
+
+        @Override
+        public List<ChildDescriptor> getChildTypes() {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public List<String> getHierarchy() {
+            return Collections.singletonList(this.getName());
+        }
+
+        @Override
+        public String getProperty(final String property) {
+            final String result;
+            if (property.equals("language")) {
+                result = this.language;
+            } else {
+                result = "";
+            }
+            return result;
+        }
+
+        @Override
+        public Builder createBuilder() {
+            return null;
+        }
+    }
+}
