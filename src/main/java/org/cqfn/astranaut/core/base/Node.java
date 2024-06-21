204384cc8d9f1c8f822757c4c85d611ea4307d69
Ivan Kniazkov <ivan@kniazkov.com>
2024-06-21 19:37:22 +0300

    Children list wrapper implementation

--- src/main/java/org/cqfn/astranaut/core/base/Node.java
+++ src/main/java/org/cqfn/astranaut/core/base/Node.java
@@ -23,9 +23,12 @@
  */
 package org.cqfn.astranaut.core.base;
 
+import java.util.AbstractList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.NoSuchElementException;
 import java.util.function.Consumer;
 
@@ -36,9 +39,12 @@ import java.util.function.Consumer;
  * easy to create custom nodes by requiring the implementation of only a few methods.
  * Additionally, the interface is designed to allow for automatic generation
  * of classes based on it.
+ * The interface is designed to be immutable, which ensures thread safety and
+ * consistency, as well as simplifying the debugging process.
  *
  * @since 1.0
  */
+@SuppressWarnings("PMD.ExcessivePublicCount")
 public interface Node extends Iterable<Node> {
     /**
      * Returns the fragment associated with the node.
@@ -147,7 +153,7 @@ public interface Node extends Iterable<Node> {
 
     @Override
     default Iterator<Node> iterator() {
-        return new NodeIterator(this);
+        return new ChildrenIterator(this, 0);
     }
 
     /**
@@ -155,7 +161,7 @@ public interface Node extends Iterable<Node> {
      *
      * @since 1.1.5
      */
-    class NodeIterator implements Iterator<Node> {
+    class ChildrenIterator implements ListIterator<Node> {
         /**
          * Node.
          */
@@ -169,10 +175,11 @@ public interface Node extends Iterable<Node> {
         /**
          * Constructor.
          * @param node Node whose children should be enumerated.
+         * @param index Starting index
          */
-        private NodeIterator(final Node node) {
+        private ChildrenIterator(final Node node, final int index) {
             this.node = node;
-            this.index = 0;
+            this.index = index;
         }
 
         @Override
@@ -188,6 +195,248 @@ public interface Node extends Iterable<Node> {
             final Node child = this.node.getChild(this.index);
             this.index = this.index + 1;
             return child;
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return this.index > 0;
+        }
+
+        @Override
+        public Node previous() {
+            if (!this.hasPrevious()) {
+                throw new NoSuchElementException();
+            }
+            this.index = this.index - 1;
+            return this.node.getChild(this.index);
+        }
+
+        @Override
+        public int nextIndex() {
+            return this.index;
+        }
+
+        @Override
+        public int previousIndex() {
+            return this.index - 1;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void set(final Node other) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void add(final Node other) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    /**
+     * Class that wraps a node and provides a list of its children..
+     *
+     * @since 2.0.0
+     */
+    @SuppressWarnings("PMD.TooManyMethods")
+    class ChildrenList implements List<Node> {
+        /**
+         * Node that is wrapped by this class.
+         */
+        private final Node node;
+
+        /**
+         * Constructor.
+         * @param node Node that will be wrapped
+         */
+        public ChildrenList(final Node node) {
+            this.node = node;
+        }
+
+        @Override
+        public int size() {
+            return this.node.getChildCount();
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return this.size() == 0;
+        }
+
+        @Override
+        public boolean contains(final Object obj) {
+            final int count = this.node.getChildCount();
+            boolean found = false;
+            for (int index = 0; index < count; index = index + 1) {
+                if (this.node.getChild(index) == obj) {
+                    found = true;
+                    break;
+                }
+            }
+            return found;
+        }
+
+        @Override
+        public Iterator<Node> iterator() {
+            return new ChildrenIterator(this.node, 0);
+        }
+
+        @Override
+        public Object[] toArray() {
+            final int count = this.node.getChildCount();
+            final Object[] array = new Object[count];
+            for (int index = 0; index < count; index = index + 1) {
+                array[index] = this.node.getChild(index);
+            }
+            return array;
+        }
+
+        @Override
+        public <T> T[] toArray(final T[] array) {
+            int count = this.node.getChildCount();
+            if (array.length < count) {
+                count = array.length;
+            }
+            for (int index = 0; index < count; index = index + 1) {
+                @SuppressWarnings("unchecked")
+                final T element = (T) this.node.getChild(index);
+                array[index] = element;
+            }
+            return array;
+        }
+
+        @Override
+        public boolean add(final Node other) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean remove(final Object obj) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean containsAll(final Collection<?> collection) {
+            boolean result = true;
+            for (final Object element : collection) {
+                if (!this.contains(element)) {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        @Override
+        public boolean addAll(final Collection<? extends Node> collection) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean addAll(final int index, final Collection<? extends Node> collection) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean removeAll(final Collection<?> collection) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean retainAll(final Collection<?> collection) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void clear() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Node get(final int index) {
+            return this.node.getChild(index);
+        }
+
+        @Override
+        public Node set(final int index, final Node element) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void add(final int index, final Node element) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Node remove(final int index) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int indexOf(final Object obj) {
+            int result = -1;
+            if (obj instanceof Node) {
+                final int count = this.node.getChildCount();
+                for (int index = 0; index < count; index = index + 1) {
+                    if (this.node.getChild(index) == obj) {
+                        result = index;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        @Override
+        public int lastIndexOf(final Object obj) {
+            int result = -1;
+            if (obj instanceof Node) {
+                for (int index = this.node.getChildCount() - 1; index >= 0; index = index - 1) {
+                    if (this.node.getChild(index) == obj) {
+                        result = index;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        @Override
+        public ListIterator<Node> listIterator() {
+            return new ChildrenIterator(this.node, 0);
+        }
+
+        @Override
+        public ListIterator<Node> listIterator(final int index) {
+            if (index < 0 || index > this.node.getChildCount()) {
+                throw new IndexOutOfBoundsException();
+            }
+            return new ChildrenIterator(this.node, index);
+        }
+
+        @Override
+        public List<Node> subList(final int start, final int end) {
+            if (start < 0 || end >= this.node.getChildCount() || start > end) {
+                throw new IndexOutOfBoundsException();
+            }
+            return new AbstractList<Node>() {
+                @Override
+                public Node get(final int index) {
+                    if (index < 0 || index >= ChildrenList.this.node.getChildCount()) {
+                        throw new IndexOutOfBoundsException();
+                    }
+                    return ChildrenList.this.node.getChild(start + index);
+                }
+
+                @Override
+                public int size() {
+                    return end - start;
+                }
+            };
         }
     }
 }
